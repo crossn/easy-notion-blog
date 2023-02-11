396d555617ff3d906427251a43ffbb5442a0ba52
herohoro <mtwd.office.25@gmail.com>
2023-02-12 00:34:07 +0900

    herohoro-sample

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { usePathname } from "next/navigation"
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import { NEXT_PUBLIC_SITE_TITLE } from '../app/server-constants'
 import styles from '../styles/header.module.css'
@@ -19,23 +19,30 @@ const Header = () => {
   ]
 
   return (
-    <header className={styles.header}>
-      <h1>
-        <Link href="/">
-          {NEXT_PUBLIC_SITE_TITLE}
-        </Link>
-      </h1>
+    <div className={styles.contents}>
+      <header className={styles.header}>
+        <h1>
+          <Link href="/">{NEXT_PUBLIC_SITE_TITLE}</Link>
+        </h1>
 
-      <ul>
-        {navItems.map(({ label, path }) => (
-          <li key={label}>
-            <Link href={path} className={pathname === path ? 'active' : null}>
-              {label}
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </header>
+        <ul className={styles.naviContent}>
+          {navItems.map(({ label, path }) => (
+            <li key={label}>
+              <Link href={path} className={pathname === path ? 'active' : null}>
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </header>
+      <div
+        style={{
+          width: '100%',
+          height: '352px',
+          backgroundColor: '#fc8b534b',
+        }}
+      ></div>
+    </div>
   )
 }
 
